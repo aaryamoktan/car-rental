d743780dea5590da89a9a783f7c1678fcc1bb2bf
aarya moktan <aaryamoktan2055@gmail.com>
2023-06-16 06:00:27 -0700

    commit first

--- src/Components/Contack/Contact.js
+++ src/Components/Contack/Contact.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <div className='contactContainer'>
+        <div className='contactForm'>
+        <h2>Contact-Form</h2>
+          <form>
+            <label>
+              Full-Name:
+            </label>
+            <br/>
+            <input type="text" name="name" value=""/>
+           <br/> <label>
+              Email-Id:
+            </label><br/>
+            <input type="text" name="name" value=""/>
+            <br/> <label>
+              Question:
+            </label><br/>
+            <input  className="question" type="text" name="name" value=""/>
+            <br/>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
